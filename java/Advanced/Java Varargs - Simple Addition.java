6b02d7c8e2a5c0156c25b4eaad5e0ce23beba521
Sasireka40 <115605176+Sasireka40@users.noreply.github.com>
2023-08-20 19:32:08 +0530

    Create Java Varargs - Simple Addition.java

---
+++ java/Advanced/Java Varargs - Simple Addition.java
@@ -0,0 +1,23 @@
+class Add 
+{
+    public static void add(int n1,int n2)
+    {
+        int x=n1+n2;
+        System.out.println(n1+"+"+n2+"="+x);
+    }
+    public static void add(int n1,int n2,int n3)
+    {
+        int x=n1+n2+n3;
+        System.out.println(n1+"+"+n2+"+"+n3+"="+x);
+    }
+    public static void add(int n1,int n2,int n3,int n4,int n5)
+    {
+        int x=n1+n2+n3+n4+n5;
+        System.out.println(n1+"+"+n2+"+"+n3+"+"+n4+"+"+n5+"="+x);
+    }
+    public static void add(int n1,int n2,int n3,int n4,int n5,int n6)
+    {
+        int x=n1+n2+n3+n4+n5+n6;
+        System.out.println(n1+"+"+n2+"+"+n3+"+"+n4+"+"+n5+"+"+n6+"="+x);
+    }
+}
