296bca2383b446ba651aaceabd39f6c4433ea066
Sasireka40 <115605176+Sasireka40@users.noreply.github.com>
2023-08-30 22:44:14 +0530

    Create "Hello World!" in C.py

---
+++ c/Introduction/"Hello World!" in C.py
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{  char s[]="Welcome to C programming.";
+   printf("Hello, World!");
+   
+   printf("\n%s",s);
+   return 0;
+}
