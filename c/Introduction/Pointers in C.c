5173caaeaf28e04a0b069e7e36180586de43fcd8
Sasireka40 <115605176+Sasireka40@users.noreply.github.com>
2023-08-30 22:48:39 +0530

    Create Pointers in C.c

---
+++ c/Introduction/Pointers in C.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function   
+    if(*a> *b) 
+    {
+        int c= *a;
+        *a= *a + *b;
+        *b= c - *b;
+    }
+    else
+    {
+        int c= *a;
+        *a= *a + *b;
+        *b= *b - c;
+    }
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
